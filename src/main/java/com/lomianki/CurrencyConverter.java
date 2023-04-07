53e5f8519a4624e77e216b3b9bf433e90dae14c2
Maciej Żelechowski <mzelechowski79@gmail.com>
2023-04-07 14:10:06 +0200

    refactoring

--- src/main/java/com/lomianki/CurrencyConverter.java
+++ src/main/java/com/lomianki/CurrencyConverter.java
@@ -12,26 +12,33 @@ public class CurrencyConverter {
     private static final String API_KEY="68e25a244e5c38d99944f3aa";
 
     public static double convert(Currency from, Currency to) {
-        StringBuilder stringBuilder = new StringBuilder();
-        String value = "0.00";
         String baseURL = "https://v6.exchangerate-api.com/v6/"+ API_KEY+"/pair/"
                 + from.getName() + "/" + to.getName();
+        String value;
         try {
-            URL url = new URL(baseURL);
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(getResponse(baseURL));
+            value = jsonNode.get("conversion_rate").toString();
+        } catch (IOException e) {
+                throw new NullPointerException(e.getMessage());
+        }
+        return Double.parseDouble(value);
+    }
+
+    public static String getResponse(String sourceURL){
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            URL url = new URL(sourceURL);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 stringBuilder.append(line);
             }
-            //System.out.println(stringBuilder);
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(stringBuilder.toString());
-            value = jsonNode.get("conversion_rate").toString();
         } catch (IOException e) {
-            System.err.println(e.getMessage());
-            System.out.println("Sprawdz czy działa server.");
+            throw new RuntimeException(e);
         }
-        return Double.parseDouble(value);
+        return stringBuilder.toString();
     }
+
 }
 
