f48a85c60442f71232264c2a8b7fc133e2da3257
Maciej Żelechowski <mzelechowski79@gmail.com>
2023-04-08 00:34:34 +0200

    remove ununsed import

--- src/main/java/com/lomianki/CurrencyModelRun.java
+++ src/main/java/com/lomianki/CurrencyModelRun.java
@@ -1,7 +1,6 @@
 package com.lomianki;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedReader;
